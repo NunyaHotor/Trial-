ceebb0bd776cb2ac62e9e598f773cf00bf157650
Mawutor <ma@sufextechnology.com>
2025-03-26 15:36:48 -0400

    my first commit

---
+++ Learn .py
@@ -0,0 +1,15 @@
+# Am learning pythons do create programs and projects for myself .
+
+#name = input ('Whats your name? ' )    .strip().title()
+
+#name = name.strip().title()
+
+
+#name = name.capitalize()
+
+#name = name.title()
+
+#
+#print('You are welcome to TeachPay', name)
+#print(f'You are welcome to TeachPay, {name}')
+
